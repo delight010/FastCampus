a0fa5debd6bc75fed56e8a41e4c29934ebbecf62
delight010 <81559026+delight010@users.noreply.github.com>
2022-05-27 22:41:24 +0900

    ViewController Life Cycle

--- iOS_30Project/Part2_Basic02_ScreenTransition/Part2_Basic02_ScreenTransition/SeguePushViewController.swift
+++ iOS_30Project/Part2_Basic02_ScreenTransition/Part2_Basic02_ScreenTransition/SeguePushViewController.swift
@@ -8,26 +8,46 @@
 import UIKit
 
 class SeguePushViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func tabBackButton(_ sender: UIButton) {
-        self.navigationController?.popViewController(animated: true) // 직전 뷰로 이동
-//        self.navigationController?.popToRootViewController(animated: true) // 네비게이션 최상단 뷰로 이동
+        print("viewDidLoad: SeguePushViewController 뷰가 로드 되었다.")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear: SeguePushViewController 뷰가 나타날 것이다.")
     }
-    */
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear: SeguePushViewController 뷰가 나타났다.")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewDidDisappear: SeguePushViewController 뷰가 사라질 것이다.")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear: SeguePushViewController 뷰가 사라졌다.")
+    }
+    
+    @IBAction func tabBackButton(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true) // 직전 뷰로 이동
+        //        self.navigationController?.popToRootViewController(animated: true) // 네비게이션 최상단 뷰로 이동
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- iOS_30Project/Part2_Basic02_ScreenTransition/Part2_Basic02_ScreenTransition/ViewController.swift
+++ iOS_30Project/Part2_Basic02_ScreenTransition/Part2_Basic02_ScreenTransition/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad: ViewController 뷰가 로드 되었다.")
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear: ViewController 뷰가 나타날 것이다.")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear: ViewController 뷰가 나타났다.")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewDidDisappear: ViewContrller 뷰가 사라질 것이다.")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear: ViewController 뷰가 사라졌다.")
+    }
+    
     @IBAction func tapCodePushButton(_ sender: UIButton) {
         guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "CodePushViewController") else { return }
         
