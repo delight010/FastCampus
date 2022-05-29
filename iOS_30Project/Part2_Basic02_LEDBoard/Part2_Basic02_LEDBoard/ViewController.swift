2b83321d10b22a39d80007ba36519d3b4944c847
delight010 <81559026+delight010@users.noreply.github.com>
2022-05-29 15:45:13 +0900

    LED 전광판 UI

---
+++ iOS_30Project/Part2_Basic02_LEDBoard/Part2_Basic02_LEDBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Part2_Basic02_LEDBoard
+//
+//  Created by Seong A Oh on 2022/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
