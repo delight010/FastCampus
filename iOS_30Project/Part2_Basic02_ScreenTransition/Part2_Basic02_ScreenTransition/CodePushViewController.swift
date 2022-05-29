44bb53c2dc814acac7d513e76314752aaf53770f
delight010 <81559026+delight010@users.noreply.github.com>
2022-05-29 10:52:07 +0900

    data 전달

--- iOS_30Project/Part2_Basic02_ScreenTransition/Part2_Basic02_ScreenTransition/CodePushViewController.swift
+++ iOS_30Project/Part2_Basic02_ScreenTransition/Part2_Basic02_ScreenTransition/CodePushViewController.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 class CodePushViewController: UIViewController {
-
+    @IBOutlet weak var nameLabel: UILabel!
+    var name: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let name = name {
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
     }
     
     @IBAction func tapBackButton(_ sender: UIButton) {
