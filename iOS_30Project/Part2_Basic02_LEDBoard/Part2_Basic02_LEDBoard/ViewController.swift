cede7d87b37d840bf5fcbb9c5931c2678eea5e8b
delight010 <81559026+delight010@users.noreply.github.com>
2022-05-29 16:09:27 +0900

    LED전광판 기능 구현

--- iOS_30Project/Part2_Basic02_LEDBoard/Part2_Basic02_LEDBoard/ViewController.swift
+++ iOS_30Project/Part2_Basic02_LEDBoard/Part2_Basic02_LEDBoard/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, LEDBoardSettingDelegate {
+    @IBOutlet weak var contentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.contentLabel.textColor = .yellow
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController { // 다운캐스팅
+            settingViewController.delegate = self
+            settingViewController.ledText = self.contentLabel.text
+            settingViewController.textColor = self.contentLabel.textColor
+            settingViewController.backgroundColor = self.view.backgroundColor ?? .black
+        }
+    }
+    
+    // 설정된 텍스트, 색상들을 받아옴
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        if let text = text {
+            self.contentLabel.text = text
+        }
+        self.contentLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
+    }
 }
 
