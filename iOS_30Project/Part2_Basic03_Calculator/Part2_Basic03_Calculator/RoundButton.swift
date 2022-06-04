a5087c1cc3ac892fedd7be887e504b54f8dc1cb1
delight010 <81559026+delight010@users.noreply.github.com>
2022-06-04 19:52:04 +0900

    Custom RoundButton 추가

---
+++ iOS_30Project/Part2_Basic03_Calculator/Part2_Basic03_Calculator/RoundButton.swift
@@ -0,0 +1,19 @@
+//
+//  RoundButton.swift
+//  Part2_Basic03_Calculator
+//
+//  Created by Seong A Oh on 2022/06/04.
+//
+
+import UIKit
+
+@IBDesignable // 스토리보드에서 모양 확인가능
+class RoundButton: UIButton {
+    @IBInspectable var isRound: Bool = false { // 스토리보드에서 변경가능
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height / 2
+            }
+        }
+    }
+}
