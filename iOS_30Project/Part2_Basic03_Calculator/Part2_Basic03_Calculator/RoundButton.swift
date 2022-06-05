b01cb2bc27914747738f54dff3c623cb9f5cfc19
delight010 <81559026+delight010@users.noreply.github.com>
2022-06-05 12:26:40 +0900

    계산기 기능 구현

--- iOS_30Project/Part2_Basic03_Calculator/Part2_Basic03_Calculator/RoundButton.swift
+++ iOS_30Project/Part2_Basic03_Calculator/Part2_Basic03_Calculator/RoundButton.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-@IBDesignable // 스토리보드에서 모양 확인가능
+//@IBDesignable // 스토리보드에서 모양 확인가능.
 class RoundButton: UIButton {
     @IBInspectable var isRound: Bool = false { // 스토리보드에서 변경가능
         didSet {
--- iOS_30Project/Part2_Basic03_Calculator/Part2_Basic03_Calculator/ViewController.swift
+++ iOS_30Project/Part2_Basic03_Calculator/Part2_Basic03_Calculator/ViewController.swift
@@ -7,13 +7,111 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Operation {
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case unknown
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = "" // numberOutputLabel에 표시될 숫자
+    var firstOperand = "" // 이전에 연산한 프로퍼티
+    var secondOperand = "" // 새로 입력된 값
+    var result = "" // 계산 결과값
+    var currentOperation: Operation = .unknown // 현재 입력된 연산자
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+//        guard let numberValue = sender.title(for: .normal) else { return }
+        guard let numberValue = sender.titleLabel?.text else { return }
+        if self.displayNumber.count < 9 {
+            self.displayNumber += numberValue
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    }
+    
+    @IBAction func tapClearButton(_ sender: UIButton) {
+        self.displayNumber = ""
+        self.firstOperand = ""
+        self.secondOperand = ""
+        self.result = ""
+        self.currentOperation = .unknown
+        self.numberOutputLabel.text = "0"
+    }
+    
+    @IBAction func tapDotButton(_ sender: UIButton) {
+        if self.displayNumber.count < 8,
+           !self.displayNumber.contains(".") {
+            self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    }
+    
+    @IBAction func tapDivideButton(_ sender: UIButton) {
+        self.operation(.Divide)
+    }
+    
+    @IBAction func tapMultipleButton(_ sender: UIButton) {
+        self.operation(.Multiply)
+    }
+    
+    @IBAction func tapSubtrackButton(_ sender: UIButton) {
+        self.operation(.Subtract)
+    }
+    
+    @IBAction func tabAddButton(_ sender: UIButton) {
+        self.operation(.Add)
+    }
+    
+    @IBAction func tapEqualButton(_ sender: UIButton) {
+        self.operation(self.currentOperation)
+    }
+    
+    func operation(_ opration: Operation) {
+        if self.currentOperation != .unknown {
+            if !self.displayNumber.isEmpty {
+                self.secondOperand = self.displayNumber
+                self.displayNumber = ""
+                
+                guard let firstOperand = Double(self.firstOperand) else { return }
+                guard let secondOperand = Double(self.secondOperand) else { return }
+                
+                switch self.currentOperation {
+                case .Add:
+                    self.result = "\(firstOperand + secondOperand)"
+                case .Subtract:
+                    self.result = "\(firstOperand - secondOperand)"
+                case .Divide:
+                    self.result = "\(firstOperand / secondOperand)"
+                case .Multiply:
+                    self.result = "\(firstOperand * secondOperand)"
+                default:
+                    break
+                }
+                
+                if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+                
+                self.firstOperand = self.result
+                self.numberOutputLabel.text = self.result
+            }
+            
+            self.currentOperation = opration
+        } else {
+            self.firstOperand = self.displayNumber
+            self.currentOperation = opration
+            self.displayNumber = ""
+        }
+        
+    }
 }
 
