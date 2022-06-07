d088d7904c9d68652d444b29a67b3e6435a3fd84
delight010 <81559026+delight010@users.noreply.github.com>
2022-06-07 20:14:18 +0900

    table view 추가

---
+++ iOS_30Project/Part2_Basic04_TodoList/Part2_Basic04_TodoList/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Part2_Basic04_TodoList
+//
+//  Created by Seong A Oh on 2022/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
