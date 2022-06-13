254cdb0e91029677aa1be506c45777212a6d01ee
delight010 <81559026+delight010@users.noreply.github.com>
2022-06-13 20:31:10 +0900

    탭바 컨트롤러 추가

---
+++ iOS_30Project/Part2_Basic04_Diary/Part2_Basic04_Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Part2_Basic04_Diary
+//
+//  Created by Seong A Oh on 2022/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
