1120b91b09936185848b3bbf4d85f5aada4f7aa2
delight010 <81559026+delight010@users.noreply.github.com>
2022-06-16 20:50:53 +0900

    일기장 스토리보드 UI 완성

---
+++ iOS_30Project/Part2_Basic04_Diary/Part2_Basic04_Diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Part2_Basic04_Diary
+//
+//  Created by Seong A Oh on 2022/06/16.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ iOS_30Project/Part2_Basic04_Diary/Part2_Basic04_Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Part2_Basic04_Diary
+//
+//  Created by Seong A Oh on 2022/06/16.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
