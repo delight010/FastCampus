dfff9a92c125bf289734828891646fb4ba6a3907
delight010 <81559026+delight010@users.noreply.github.com>
2022-06-18 21:21:06 +0900

    일기작성 페이지 기능 구현(1)
    - 모든 항목 입력해야 등록버튼 작동
    - 값을 입력받을 때마다 상태를 감지

--- iOS_30Project/Part2_Basic04_Diary/Part2_Basic04_Diary/WriteDiaryViewController.swift
+++ iOS_30Project/Part2_Basic04_Diary/Part2_Basic04_Diary/WriteDiaryViewController.swift
@@ -8,22 +8,81 @@
 import UIKit
 
 class WriteDiaryViewController: UIViewController {
-
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIBarButtonItem!
+    
+    private let datePicker = UIDatePicker()
+    private var diaryDate: Date?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.configureContentsTextView()
+        self.configureDatePicker()
+        self.configureInputField()
+        self.confirmButton.isEnabled = false // 등록버튼 비활성화
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureContentsTextView() {
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
+        self.contentTextView.layer.borderColor = borderColor.cgColor
+        self.contentTextView.layer.borderWidth = 0.5
+        self.contentTextView.layer.cornerRadius = 5.0
     }
-    */
+    
+    private func configureDatePicker() {
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .wheels
+        self.datePicker.addTarget(self, action: #selector(datePickerValueDidChange(_:)), for: .valueChanged)
+        //.valueChanged : 값이 변경될 때 작동
+        self.dateTextField.inputView = self.datePicker
+        // 하단에 키보드 대신 datePicker를 표시
+    }
+    
+    private func configureInputField() {
+        self.contentTextView.delegate = self
+        self.titleTextField.addTarget(self, action: #selector(titleTextFieldDidChange(_:)), for: .editingChanged)
+        // .editingChanged : 수정될 때마다 실행
+        self.dateTextField.addTarget(self, action: #selector(dateTextFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    @objc private func titleTextFieldDidChange(_ textField: UITextField) {
+        self.validateInputField() // 제목 입력될 때마다 실행
+    }
+    
+    @objc private func dateTextFieldDidChange(_ textField: UITextField) {
+        self.validateInputField() // 날짜가 입력될 때마다 실행
+    }
+    
+    @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
+    }
+    
+    @objc private func datePickerValueDidChange(_ datePicker: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 MM월 dd일(EEEEE)"
+        // (EEEEE) : 요일을 한글자만 표현
+        formatter.locale = Locale(identifier: "ko_KR") // 한국어로 표현
+        self.diaryDate = datePicker.date
+        self.dateTextField.text = formatter.string(from: datePicker.date)
+        self.dateTextField.sendActions(for: .editingChanged) // textViewDidChange 실행
+    }
+    
+    // 화면 터치 시 키보드 사라짐
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // 등록 버튼의 활성화 여부 판단
+    private func validateInputField() {
+        self.confirmButton.isEnabled = !(self.titleTextField.text?.isEmpty ?? true) && !(self.dateTextField.text?.isEmpty ?? true) && !self.contentTextView.text.isEmpty
+    }
 
+}
+
+extension WriteDiaryViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        // 일기장 내용이 입력될 때마다 실행
+        self.validateInputField() // 등록버튼 활성화 여부 판단
+    }
 }
